191246d28f1c26a419609c9c21314a962a2e50e7
Aqib Suhail <59912182+geeqib23@users.noreply.github.com>
2024-04-03 15:57:50 +0530

    connection established and data received

--- static/js/main.js
+++ static/js/main.js
@@ -1,46 +1,62 @@
 const controlButton = document.getElementById("controlButton");
 const deviceNameInput = document.getElementById("deviceNameInput");
 const connectionStatus = document.getElementById("connectionStatus");
+const bs = document.getElementById('bluetoothState')
+const outputText = document.getElementById("output");
 
 function isWebBluetoothEnabled() {
   if (!navigator.bluetooth) {
-      console.log('Web Bluetooth API is not available in this browser!')
-      document.getElementById('bluetoothState').innerText = 'Not available'
-      return false
+    console.log('Web Bluetooth API is not available in this browser!')
+    bs.classList.replace("text-muted", "text-danger")
+    bs.innerText = 'Not available'
+    return false
   }
-  document.getElementById('bluetoothState').innerText = 'Available'
+  bs.classList.replace("text-muted", "text-success")
+  bs.innerText = 'Available'
   return true
 }
 document.addEventListener('DOMContentLoaded', function() {
   isWebBluetoothEnabled()
 }, false);
+function handleCharacteristicValueChanged(event) {
+  const value = event.target.value;
+  console.log(String.fromCharCode.apply(null, new Uint8Array(value.buffer)));
+  outputText.textContent = String.fromCharCode.apply(null, new Uint8Array(value.buffer));;
+}
 var error_code
 async function BLEManager() {
-    connectionStatus.textContent = "SEARCHING"; 
+    connectionStatus.textContent = "SEARCHING";
+    connectionStatus.classList.replace("text-muted", "text-secondary") 
     error_code = 0
     try {
       const device = await navigator.bluetooth.requestDevice({
-        filters: [{
-          name: deviceNameInput.value,
-          services: ['6e400001-b5a3-f393-e0a9-e50e24dcca9e']
-        }]
+        acceptAllDevices:true,
+        optionalServices: ['6e400001-b5a3-f393-e0a9-e50e24dcca9e']
       });
       error_code = 1
       const connectedDevice = await device.gatt.connect();
       connectionStatus.textContent = "CONNECTED";
+      connectionStatus.classList.replace("text-secondary", "text-primary") 
+      outputText.innerHTML = "Predicting..."
+      outputText.classList.replace("text-muted","text-success")
       error_code = 2
-      const outputService = await connectedDevice.getPrimaryService("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
+      const service = await connectedDevice.getPrimaryService("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
       console.log("Services obtained")
       error_code = 3
-      const outputCharacteristic = await outputService.getCharacteristic("6e400003-b5a3-f393-e0a9-e50e24dcca9e");
+      const characteristic = await service.getCharacteristic("6e400003-b5a3-f393-e0a9-e50e24dcca9e");
       console.log("Characteristics discovered")
       error_code = 4
-      const output = await outputCharacteristic.readValue();
-      console.log(output)
-      const outputConv = output.getUint8(0);
-      console.log(outputConv)
-      const outputCharge = document.getElementById("output");
-      outputCharge.textContent = outputConv.toString();
+      // const output = await characteristic.readValue()
+      // console.log(output)
+      // const t = output.buffer
+      // console.log(t)
+      // consoutputT text = document.getElementById("output")
+      // text.textContent = output.toString()
+      const output = await characteristic.startNotifications();
+      error_code = 5
+      characteristic.addEventListener('characteristicvaluechanged', handleCharacteristicValueChanged)
+      error_code = 6
+      console.log("notification started successfully")
     }
     catch(e) {
       console.log("err_code",error_code)
